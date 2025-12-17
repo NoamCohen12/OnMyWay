b30e8c48cc65e836e190b410ee6df2864178a74a
yairMar1 <yairchai@gmail.com>
2025-12-17 12:56:13 +0200

    Create Frontend file

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header'
+import Map from './components/Map'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <Map />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
